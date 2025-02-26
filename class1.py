5d7e4b1d11dbafa246759661db191b089a8568e0
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2025-02-26 22:19:38 +0500

    python-first-assigment

---
+++ class1.py
@@ -0,0 +1,70 @@
+print("hello world!1")
+
+# def main():
+#     print("hello world from student")
+
+name : str = "Governor Intiative Artifical intelligence and computing"
+print( "GIAIC stand for : " ,name)
+
+# string
+# integer(number)
+# float(.decimal)
+# boolean
+# list(array)
+# tuple(imutible type similar list)
+# dictionary(object)
+# set 
+# non(none type)
+# frozen set 
+# byte array
+
+# String
+
+snake_case : str = "sir asharib ali"
+print(snake_case)
+
+id_card :str = """Name : Zari 
+Father_name : Ahmed"""
+print(id_card)
+
+
+# number
+
+roll_num :int = 20
+print(type(roll_num))
+print(roll_num)
+
+# float
+
+float_num :float = 3.5
+print(float_num)
+
+
+# booleaan
+
+boolean :bool = False
+print(type(boolean))
+print(boolean)
+
+
+# list
+
+grocery: list[str] = ["apple", "cherry"]
+print(type(grocery))
+print(grocery[-1])
+
+
+# tuple
+
+tuple_data : tuple = ("mango", "stawberry")# immutabe jo change nhi hoti value
+print(tuple_data)
+
+# dictonary
+
+
+user_data : dict[str,str] = {
+    "name ": "hamza",
+    "fathername" : "syed"
+}
+print(user_data["fathername"])
+
---
+++ module.py
@@ -0,0 +1,2 @@
+from class1 import main
+main()
